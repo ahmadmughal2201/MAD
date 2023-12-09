1206375614f6a30bf547dd3f46a9bdb448b5e31e
Ahmad Mughal <ahmadmughal2201@gmail.com>
2023-12-09 16:20:19 +0500

    Chapter 13

---
+++ chapter13/lib/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        initialData: [],
+        future: _loadJournals(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          return !snapshot.hasData
+              ? Center(child: CircularProgressIndicator())
+              : _buildListViewSeparated(snapshot);
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Padding(padding: const EdgeInsets.all(24.0)),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add Journal Entry',
+        child: Icon(Icons.add),
+        onPressed: () {
+          _addOrEditJournal(add: true, index: -1, journal: Journal());
+        },
+      ),
+    );
+  }
+}
